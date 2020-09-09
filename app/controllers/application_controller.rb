90f3b6d714a8fe7f268d14300ecbb339cdf8e697
alex0033 <chanchanmen0807@icloud.com>
2020-09-09 14:03:37 +0000

    finish layout of devise/users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction])
   end
-
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  def after_sign_up_path_for(resource)
-    root_path
-  end
-
-  def after_update_path_for(resource)
-    root_path
-  end
 end
