a4c9a14a0e77c7755cf2e1f06c357c6c6ee2081c
alex0033 <chanchanmen0807@icloud.com>
2020-09-09 23:02:26 +0000

    add system spec for devise/user

---
+++ spec/support/capybara.rb
@@ -0,0 +1,17 @@
+Capybara.register_driver :selenium_chrome_headless do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-dev-shm-usage'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
--- spec/system/devise/registrations_spec.rb
+++ spec/system/devise/registrations_spec.rb
@@ -1,4 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe "Devise/Registrations", type: :system do
+  let(:user) { create(:user, password: "password") }
+
+  it "makes user" do
+    visit root_path
+    within(:css, '.header') do
+      click_on "アカウント作成"
+    end
+    expect(page).to have_current_path new_user_registration_path
+    # 不正な入力
+    within(:css, '.form-box') do
+      fill_in('user_email', with: "invalid")
+      fill_in('user_password', with: "password")
+      fill_in('user_password_confirmation', with: "password")
+      click_on "アカウント作成"
+      expect(page).to have_content("アカウント作成")
+    end
+    # 正しい入力
+    within(:css, '.form-box') do
+      fill_in('user_email', with: "valid@example.com")
+      fill_in('user_password', with: "password")
+      fill_in('user_password_confirmation', with: "password")
+      click_on "アカウント作成"
+    end
+    expect(page).to have_current_path root_path
+    # flash_massage??
+  end
+
+  it "update user" do
+    sign_in user
+    visit edit_user_registration_path
+    name_changed = user.name + "change"
+    # 不正な入力
+    within(:css, '.form-box') do
+      fill_in('user_name', with: name_changed)
+      fill_in('user_current_password', with: "invalid")
+      click_on "更新"
+      expect(page).to have_content("プロフィール編集")
+    end
+    # ここでエラーメッセージ確認？？
+    # 正しい入力
+    within(:css, '.form-box') do
+      fill_in('user_email', with: "valid@example.com")
+      fill_in('user_current_password', with: "password")
+      click_on "更新"
+    end
+    expect(page).to have_current_path root_path
+    # flash_massage??
+  end
+
+  it "delete user", js: true do
+    sign_in user
+    visit edit_user_registration_path
+    within(:css, '.form-box') do
+      page.accept_confirm("本当に削除しますか？") do
+        click_on "アカウント削除"
+      end
+    end
+    expect(page).to have_current_path root_path
+  end
 end
