706c67229dc16b3f37c441c07ee5d3ab7a2bb529
alex0033 <chanchanmen0807@icloud.com>
2020-09-10 00:07:15 +0000

    set users(not devise) controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'basic_pages#home'
   get 'policy', to: 'basic_pages#policy'
+  devise_for :users
+  resources :users, only: :show
 end
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "#show" do
+    let!(:user) { create(:user, name: "name") }
+
+    context "when user not signed_in" do
+      before do
+        get user_path(user)
+      end
+
+      it { expect(response.status).to eq(302) }
+      it { expect(response).to redirect_to new_user_session_path }
+    end
+
+    context "when user signed_in" do
+      let!(:other_user) { create(:user, name: "other_name") }
+
+      context "when user tries to uncorrect path" do
+        before do
+          sign_in user
+          get user_path(user.id + other_user.id)
+        end
+
+        it { expect(response.status).to eq(302) }
+        it { expect(response).to redirect_to root_path }
+      end
+
+      context "when user tries to correct path" do
+        before do
+          sign_in user
+          get user_path(other_user)
+        end
+
+        it { expect(response.status).to eq(200) }
+        it { expect(response.body).to include(other_user.name) }
+      end
+    end
+  end
+end
