91645358ffbe5dc08ec134de3a24673a82e3f6ee
alex0033 <chanchanmen0807@icloud.com>
2020-09-14 07:32:22 +0000

    fix default_scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,11 +5,10 @@ class Job < ApplicationRecord
   validates :pay,         presence: true, length: { maximum: 30 }
   validates :explanation, presence: true, length: { maximum: 2000 }
 
-  default_scope -> { order(created_at: :desc) }
-
   def self.search(search)
     Job.where("title LIKE ?", "%#{search}%").
       or(where("pay LIKE ?", "%#{search}%")).
-      or(where("explanation LIKE ?", "%#{search}%"))
+      or(where("explanation LIKE ?", "%#{search}%")).
+      order(created_at: :desc)
   end
 end
