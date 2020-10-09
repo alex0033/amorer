e6b4a83a528121d91e0aaf18dc0b55bc19908260
alex0033 <chanchanmen0807@icloud.com>
2020-10-09 10:25:00 +0000

    set model for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :timeoutable, :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
+         :timeoutable, :omniauthable, omniauth_providers: [:facebook]
 
   has_many :jobs,    dependent: :destroy
   has_many :entries, dependent: :destroy
@@ -25,6 +25,17 @@ class User < ApplicationRecord
   validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                     size: { less_than: 5.megabytes, message: 'のデータサイズが大きすぎます' }
 
+  validates :provider, uniqueness: { scope: :uid, case_sensitive: true }, allow_nil: true
+
+  with_options presence: true, if: :was_attached? do
+    validates :x
+    validates :y
+    validates :height
+    validates :width
+  end
+
+  validate :can_auth?
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
@@ -40,13 +51,6 @@ class User < ApplicationRecord
     user
   end
 
-  with_options presence: true, if: :was_attached? do
-    validates :x
-    validates :y
-    validates :height
-    validates :width
-  end
-
   def count_not_read_messages
     Message.where(receiver: self).where(read: false).size
   end
@@ -57,5 +61,23 @@ class User < ApplicationRecord
 
   def was_attached?
     image.attached?
+  end
+
+  def email_required?
+    !has_auth?
+  end
+
+  def has_auth?
+    provider.present? && uid.present?
+  end
+
+  def auth_is_complete_nil?
+    provider.nil? && uid.nil?
+  end
+
+  def can_auth?
+    unless has_auth? || auth_is_complete_nil?
+      errors.add(:base, "認証エラーが発生しています")
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     sequence(:name) { |n| "sample#{n}" }
     sequence(:email) { |n| "sample#{n}@example.com" }
     password { "password" }
+
+    factory :user_with_facebook do
+      provider { "facebook" }
+      sequence(:uid) { |n| "00000#{n}" }
+    end
   end
 end
