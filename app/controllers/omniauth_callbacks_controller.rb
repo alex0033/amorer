495675e724f3522e6b98da9f9e499503ff683e1f
alex0033 <chanchanmen0807@icloud.com>
2020-10-10 02:15:03 +0000

    set request spec for facebook login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
+    if @user&.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
       sign_in_and_redirect @user, event: :authentication
     else
       session['devise.facebook_data'] = request.env['omniauth.auth'].except('extra')
+      flash[:warnig] = "そのFacebookアカウントでは、ログインできませんでした。" if @user.nil?
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
